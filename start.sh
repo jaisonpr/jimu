bca45d88fcdbe8521c6aec7cbce09cb8d1c4dade
Jaison PR <jaisonpr@gmail.com>
2022-07-20 12:55:59 -0300

    Removing deprecated mode to open a new terminal

--- start.sh
+++ start.sh
@@ -2,7 +2,7 @@
 
 sudo service mongod stop
 
-gnome-terminal -x bash -c "cd ./database/mongodb/; sudo ./start.sh; exec bash"
-gnome-terminal -x bash -c "cd ./backend/; npm start; exec bash"
-gnome-terminal -x bash -c "cd ./frontend/; npm start; exec bash"
+gnome-terminal -- bash -c "cd ./database/mongodb/; sudo ./start.sh; exec bash"
+gnome-terminal -- bash -c "cd ./backend/; npm start; exec bash"
+gnome-terminal -- bash -c "cd ./frontend/; npm start; exec bash"
 
